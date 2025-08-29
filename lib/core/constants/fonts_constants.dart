10aa54a42517a5184d8cdee79fbaad5796439689
Dhruvik <dhruvprj910@gmail.com>
2025-08-30 00:55:49 +0530

    Constants Added

---
+++ lib/core/constants/fonts_constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyles {
+  // Regular Roboto
+  static final robotoRegular = GoogleFonts.roboto(
+    fontWeight: FontWeight.w400,
+  );
+
+  // Medium Roboto
+  static final robotoMedium = GoogleFonts.roboto(
+    fontWeight: FontWeight.w500,
+  );
+
+  // Bold Roboto
+  static final robotoBold = GoogleFonts.roboto(
+    fontWeight: FontWeight.w700,
+  );
+
+}
---
+++ lib/core/constants/http_constant.dart
@@ -0,0 +1,5 @@
+const String baseUrl = 'http://3.7.81.243/projects/plie-api/public/api';
+
+
+const String loginApi = '${baseUrl}/login';
+const String listingApi = '${baseUrl}/events-listing';
